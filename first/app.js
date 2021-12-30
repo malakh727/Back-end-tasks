a06d99ba6500d0255774fbdfa06d6974981ef779
malakh727 <76812123+malakh727@users.noreply.github.com>
2021-12-30 21:34:41 +0200

    Delete first directory

--- first/app.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello world!!!'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
-console.log('listening on port 3000...');
